c32a5015fcda1b6183883c8b40c1d41e5b1c1f61
carolina <carolinacapetilloc@gmail.com>
2021-01-18 15:07:36 -0800

    adds routes

---
+++ src/Pages/Error/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Error.css';
+
+const Error = () => {
+    return (
+        <div className="Error">
+            <h1>Oops! Page not found</h1>
+        </div>
+    )
+}
+
+export default Error
